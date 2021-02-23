8e60afd39ccb042d2f1598f62d345dbdd8720a51
Leong Khei Hua <donleong94@gmail.com>
2021-02-24 02:21:00 +0800

    - UI update in now playing page - Minor code changes

--- app/src/main/java/com/loong/ihms/activity/AlbumDetailsActivity.kt
+++ app/src/main/java/com/loong/ihms/activity/AlbumDetailsActivity.kt
@@ -1,7 +1,6 @@
 package com.loong.ihms.activity
 
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
